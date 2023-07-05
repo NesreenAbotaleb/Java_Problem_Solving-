106c87a773544e6d03a3cb4dab74e63b472443d1
NesreenAbotaleb <109898670+NesreenAbotaleb@users.noreply.github.com>
2023-07-05 18:21:51 +0300

    Delete EX_05_GUI.java

--- EX_05_GUI.java
+++
@@ -1,9 +0,0 @@
-package Ch_01;
-import javax.swing.JOptionPane;
-public class EX_05_GUI {
-    public static void main(String[] args) {
-        
-        JOptionPane.showMessageDialog(null, ((9.5 * 4.5) -
-                (2.5 * 3)) / (45.5 - 3.5));
-    }
-}
