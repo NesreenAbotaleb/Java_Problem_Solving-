39a04c66a9a4c133e6baab8c672999036a89377f
NesreenAbotaleb <mohamedaboelhawey@gmail.com>
2023-07-05 18:14:06 +0300

    first commit

--- EX_06_GUI.java
+++ EX_06_GUI.java
@@ -4,5 +4,6 @@ public class EX_06_GUI {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, 1 + 2 + 3 + 4 + 5 + 6
                 + 7 + 8 + 9);
+        
     }
 }
