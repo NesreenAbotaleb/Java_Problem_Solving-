d641e620b9d5a3f2700d58e48a076c8d1b85003f
NesreenAbotaleb <109898670+NesreenAbotaleb@users.noreply.github.com>
2023-07-16 12:02:06 +0300

    Update and rename EX_04.java to EX01_04.java

--- EX01_04.java
+++ EX01_04.java
@@ -1,6 +1,7 @@
-package Ch_01;
+package CH01;
 
-public class EX_04 {
+public class EX01_04 {
+    
     public static void main (String [] args){
         System.out.println(" a    a^2   a^3 \n 1     1     1\n 2     4     8\n"
                 + " 3     9    27\n 4    16    64");
