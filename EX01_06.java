babc65c989ee9c91519e5978f051728e10beddeb
NesreenAbotaleb <109898670+NesreenAbotaleb@users.noreply.github.com>
2023-07-16 12:04:20 +0300

    Update and rename EX_06.java to EX01_06.java

--- EX01_06.java
+++ EX01_06.java
@@ -1,6 +1,7 @@
-package Ch_01;
+package CH01;
 
-public class EX_06 {
+public class EX01_06 {
+    
     public static void main(String [] args){
         System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
     }
