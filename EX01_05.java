ce6e714e296b0b906e15d1844a23da0af45c3bdc
NesreenAbotaleb <109898670+NesreenAbotaleb@users.noreply.github.com>
2023-07-16 12:03:37 +0300

    Update and rename EX_05.java to EX01_05.java

--- EX01_05.java
+++ EX01_05.java
@@ -1,6 +1,7 @@
-package Ch_01;
+package CH01;
 
-public class EX_05 {
+public class EX01_05 {
+    
     public static void main(String [] args){
         System.out.println(((9.5 * 4.5) - (2.5 * 3)) / (45.5 - 3.5));
     }
