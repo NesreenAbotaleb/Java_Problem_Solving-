581c5891bd5a2b1926597dd45dcde6b56ce9000d
NesreenAbotaleb <109898670+NesreenAbotaleb@users.noreply.github.com>
2023-07-16 11:57:32 +0300

    Update and rename EX_01.java to EX01_01.java

--- EX01_01.java
+++ EX01_01.java
@@ -1,7 +1,7 @@
-package Ch_01;
+package Ch01;
 
-
-public class EX_01 {
+public class EX01_01 {
+    
     public static void main(String[]args){
     
         System.out.println("Welcome to java\n" + " Welcome to computer science\n " 
